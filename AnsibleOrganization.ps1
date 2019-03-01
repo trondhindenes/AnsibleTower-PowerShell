57bf3a12c2fd2f34c4c97bea473b103b14b39a70
Chris Duck <gpduck@whatsupduck.net>
2019-03-01 16:33:13 -0600

    Move Get-AnsibleOrganization out to separate file

--- AnsibleOrganization.ps1
+++ AnsibleOrganization.ps1
@@ -1,40 +1,3 @@
-function Get-AnsibleOrganization
-{
-    [CmdletBinding()]
-    Param (
-        [String]$Name,
-        [Parameter(ValueFromPipelineByPropertyName=$true)]
-        [int]$id
-    )
-
-    if ($id)
-    {
-        $Return = Invoke-GetAnsibleInternalJsonResult -ItemType "organizations" -Id $id
-    }
-    Else
-    {
-        $Return = Invoke-GetAnsibleInternalJsonResult -ItemType "organizations"
-    }
-    
-
-    if (!($Return))
-    {
-        #Nothing returned from the call
-        Return
-    }
-    $returnobj = @()
-    foreach ($jsonorg in $return)
-    {
-        #Shift back to json and let newtonsoft parse it to a strongly named object instead
-        $jsonorgstring = $jsonorg | ConvertTo-Json
-        $org = $JsonParsers.ParseToOrganization($jsonorgstring)
-        $returnobj += $org; $org = $null
-
-    }
-    #return the things
-    $returnobj
-}
-
 Function New-AnsibleOrganization
 {
     [CmdletBinding()]
@@ -56,4 +19,3 @@ Function New-AnsibleOrganization
     }
     
 }
-
