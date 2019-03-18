b377249945e51006a41a38aff0f0ae67f327e1ac
Chris Duck <gpduck@whatsupduck.net>
2019-03-18 14:05:10 -0500

    Add support for groups as members

--- src/ExportedFunctions/Add-AnsibleGroupMember.ps1
+++ src/ExportedFunctions/Add-AnsibleGroupMember.ps1
@@ -5,54 +5,113 @@ function Add-AnsibleGroupmember {
         [Parameter(Mandatory=$true)]
         $Group,
 
-        [Parameter(Mandatory=$true)]
-        [object[]]$Members,
+        [Parameter(Mandatory=$true,ParameterSetName="AddHost")]
+        [object[]]$Hosts,
+
+        [Parameter(Mandatory=$true,ParameterSetName="AddGroup")]
+        [object[]]$ChildGroups,
+
+        $Inventory,
 
         $AnsibleTower = $Global:DefaultAnsibleTower
     )
     begin {
+        $InventoryParam = @{}
+        if($Inventory) {
+            $InventoryParam["Inventory"] = $Inventory
+        } elseif($Group.Contains("/")) {
+            $InventoryParam["Inventory"],$Group = $Group.Split("/")
+        }
         switch($Group.GetType().Fullname) {
             "AnsibleTower.Group" {
                 #do nothing
             }
             "System.String" {
-                $Group = Get-AnsibleGroup -Name $Group -AnsibleTower $AnsibleTower
+                $Group = Get-AnsibleGroup -Name $Group @InventoryParam -AnsibleTower $AnsibleTower
             }
             "System.Int32" {
                 $Group = Get-AnsibleGroup -Id $Group -AnsibleTower $AnsibleTower
             }
             default {
                 Write-Error "Unknown type passed as -Group ($_).  Suppored values are String, Int32, and AnsibleTower.Group." -ErrorAction Stop
-                return
+                break
             }
+        }
+        if($Group.Count -gt 1) {
+            $GroupList = ($Group | ForEach-Object {
+                "$($_.Inventory.Name)/$($_.Name)"
+            }) -Join ", "
+            Write-Error "Multiple target groups found: $GroupList"
+            break
+        }
+        if(!$InventoryParam.ContainsKey("inventory")) {
+            $inventoryParam["Inventory"] = $Group.Inventory
         }
     }
     process {
-        $Members | ForEach-Object {
-            $Member = $_
-            switch($Member.GetType().Fullname) {
-                "AnsibleTower.Host" {
-                    $AnsibleTower = $Member.AnsibleTower
+        if($PSCmdlet.ParameterSetName -eq "AddHost") {
+            $Hosts | ForEach-Object {
+                $ThisHost = $_
+                switch($ThisHost.GetType().Fullname) {
+                    "AnsibleTower.Host" {
+                        $AnsibleTower = $ThisHost.AnsibleTower
+                    }
+                    "System.String" {
+                        $ThisHost = Get-AnsibleHost -Name $ThisHost @InventoryParam -AnsibleTower $AnsibleTower
+                    }
+                    "System.Int32" {
+                        $ThisHost = Get-AnsibleHost -Id $ThisHost -AnsibleTower $AnsibleTower
+                    }
+                    default {
+                        Write-Error "Unknown type passed as -Hosts ($_).  Suppored values are String, Int32, and AnsibleTower.Host." -ErrorAction Stop
+                        return
+                    }
                 }
-                "System.String" {
-                    $Member = Get-AnsibleHost -Name $Member -AnsibleTower $AnsibleTower
-                }
-                "System.Int32" {
-                    $Member = Get-AnsibleHost -Id $Member -AnsibleTower $AnsibleTower
-                }
-                default {
-                    Write-Error "Unknown type passed as -Inventory ($_).  Suppored values are String, Int32, and AnsibleTower.Inventory." -ErrorAction Stop
+                if(!$ThisHost) {
+                    Write-Error "Unable to locate host by '$_'"
                     return
                 }
-            }
-            $MemberGroupUrl = Join-AnsibleUrl $Member.Url, 'groups'
-            $HostName = $Member.Name
+                $HostGroupUrl = Join-AnsibleUrl $ThisHost.Url, 'groups'
+                $HostName = $ThisHost.Name
 
-            $GroupName = $Group.Name
-            if($PSCmdlet.ShouldProcess($AnsibleTower.ToString(), "Add host '$HostName' to group '$GroupName'")) {
-                Invoke-AnsibleRequest -AnsibleTower $AnsibleTower -FullPath $MemberGroupUrl -Method POST -Body (
-                    ConvertTo-Json @{ id = $Group.Id}
-                )
+                $GroupName = $Group.Name
+                if($PSCmdlet.ShouldProcess($AnsibleTower.ToString(), "Add host '$HostName' to group '$GroupName'")) {
+                    Invoke-AnsibleRequest -AnsibleTower $AnsibleTower -FullPath $HostGroupUrl -Method POST -Body (
+                        ConvertTo-Json @{ id = $Group.Id}
+                    )
+                }
+            }
+        } else {
+            $ChildGroups | ForEach-Object {
+                $ThisGroup = $_
+                switch($ThisGroup.GetType().Fullname) {
+                    "AnsibleTower.Group" {
+                        $AnsibleTower = $ThisGroup.AnsibleTower
+                    }
+                    "System.String" {
+                        $ThisGroup = Get-AnsibleGroup -Name $ThisGroup @InventoryParam -AnsibleTower $AnsibleTower
+                    }
+                    "System.Int32" {
+                        $ThisGroup = Get-AnsibleGroup -Id $ThisGroup -AnsibleTower $AnsibleTower
+                    }
+                    default {
+                        Write-Error "Unknown type passed as -Group ($_).  Suppored values are String, Int32, and AnsibleTower.Group." -ErrorAction Stop
+                        return
+                    }
+                }
+                if(!$ThisGroup) {
+                    Write-Error "Unable to locate child group by '$_'"
+                    return
+                }
+                $GroupChildrenUrl = Join-AnsibleUrl $ThisGroup.Url, 'children'
+                $ThisGroupName = $ThisGroup.Name
+
+                $GroupName = $Group.Name
+                if($PSCmdlet.ShouldProcess($AnsibleTower.ToString(), "Add group '$ThisGroupname' to group '$GroupName'")) {
+                    Invoke-AnsibleRequest -AnsibleTower $AnsibleTower -Fullpath $GroupChildrenUrl -Method POST -Body (
+                        ConvertTo-Json @{ id = $Group.Id}
+                    )
+                }
             }
         }
     }
